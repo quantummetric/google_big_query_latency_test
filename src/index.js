404c8ce3df96b4213a3770b9134c159b99aa9c99
Nick Beaulieu <nbeaulieu@quantummetric.com>
2017-02-17 13:42:56 -0700

    added stress testing code refactored project id to environment variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import BigQuery from './BQRestAPI'
+import ProgressBar from "progress"
 import "console.table"
 
-const projectId = 'PROJECT_ID';
+const projectId = process.env.GOOGLE_PROJ_ID;
 
 const genQuery  = () => `SELECT ${Math.floor(Math.random() * 100)}`;
 const bigQuery = BigQuery({projectId});
@@ -23,27 +24,41 @@ function summaryStats(arr) {
     }
 }
 
-async function runTrials(trialCount) {
-    const timingPromises = [...new Array(trialCount)].map(i => testQuery());
-    const timings = await Promise.all(timingPromises);
-    const keys = ['completeTime'];
-    const stats = keys.map(k => ({key: k, ...summaryStats(timings.map(t => t[k]))}));
-    console.log(`Timings for ${trialCount} concurrent queries`);
-    console.table(stats);
-    return stats;
+async function stressTest(concurrencyCount, trialCount) {
+    const timings = new Array(trialCount);
+    const bar = new ProgressBar('[:bar] :percent', {total: trialCount});
+    for (let i = 0; i < trialCount; i++) {
+        bar.tick();
+        timings[i] = await Promise.all(
+            [... new Array(concurrencyCount)].map(i => testQuery())
+        );
+    }
+    const timingsGrid = timings.map(trials => trials.map(t => t.completeTime));
+    const timingsArray = [].concat.apply([], timingsGrid);
+    const min = Math.min(...timingsArray);
+    const max = Math.max(...timingsArray);
+    const avg = timingsArray.reduce((a, b) => a + b, 0) / timingsArray.length;
+    console.log(`Overall Stats\n\tMin:${min}\n\tMax:${max}\n\tAvg:${avg}\nTrial Stats:`);
+    console.table(timingsGrid.map((row, idx) => ({trial: idx, ...summaryStats(row)})));
+    return timingsGrid;
 }
 
 async function main() {
-    const trialSizes = [1,2,4,8,16,32,64, 128];
-    console.log(`Testing BQ on randomized "SELECT {n}" statements with ${trialSizes.join(', ')} concurrent queries`);
-    try {
-        for (let size of trialSizes) {
-            const stats = await runTrials(size);
-        }
+    const concurrencyCount = process.env.CCOUNT || 32; // Number of concurrent queries to run
+    const trialCount = process.env.TCOUNT || 100; // Number of iterations to run the test
 
+    if (!projectId) {
+        console.error('Need to provide project id in GOOGLE_PROJ_ID environment variable!');
+        return;
+    }
+    try {
+        console.log(`Stress test of ${concurrencyCount} concurrent queries for ${trialCount} trials`);``
+        await stressTest(concurrencyCount, trialCount);
     } catch (err) {
         console.error(`${err.message} ${err.stack}`);
     }
 }
+
+
 
 main();
